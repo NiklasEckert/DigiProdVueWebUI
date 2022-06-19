c9a7b1aafbe4f8b086ad6c4cef0a5e1bc5b6f366
niklaseckert <niklas.eckert.1807@gmail.com>
2022-06-19 18:26:26 +0200

    Add: Added font family Avenir.

--- src/App.vue
+++ src/App.vue
@@ -20,5 +20,9 @@ export default {
 </script>
 
 <style>
-
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
 </style>
