bd4c9a75870739c8bd3da2553abce23a6e04c6dc
niklaseckert <niklas.eckert.1807@gmail.com>
2022-06-19 17:21:50 +0200

    Change: Changed background colors to amber.

--- src/components/navigation/SidebarNav.vue
+++ src/components/navigation/SidebarNav.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="h-screen w-64 bg-indigo-900 text-white py-12 px-6">
+  <div class="h-screen w-64 bg-amber-200 text-black py-12 px-6">
     <h1 class="text-2xl font-bold mb-7">IMACS DigiProd</h1>
     <ul>
       <li v-for="item in navItems" :key="item.name">
-        <router-link :to="item.path" active-class="bg-indigo-600 hover:bg-indigo-600 font-bold drop-shadow-lg" class="block py-2 px-3 mb-1 hover:bg-indigo-600/50 hover:cursor-pointer rounded-md">{{ item.name }}</router-link>
+        <router-link :to="item.path" active-class="bg-amber-400 hover:bg-amber-400 font-bold drop-shadow-lg text-white" class="block py-2 px-3 mb-1 hover:bg-amber-400/50 hover:cursor-pointer rounded-md">{{ item.name }}</router-link>
       </li>
     </ul>
   </div>
