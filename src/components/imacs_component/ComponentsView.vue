ad8ace442f6d041178488650f73de6c6a9d07489
ichbinbenni <benedikt@ibb-games.de>
2022-06-26 12:55:56 +0200

    Change to use searchState

--- src/components/imacs_component/ComponentsView.vue
+++ src/components/imacs_component/ComponentsView.vue
@@ -2,19 +2,21 @@
   <SelectionSidebar
       headline="Components"
       search-bar-placeholder="Name, article or order number"
+      :search-text="this.componentSearchState.key"
       @searchKeyEntered="(key) => fetchSearch(key)"
       @addButtonPressed="() => $router.push({ name: 'component', query: { viewMode: 'creation' }})"
   >
     <div v-if="loading" class="text-center">LOADING</div>
-    <ComponentTable :componentList="componentList" v-if="!loading"/>
+    <ComponentTable :componentList="this.componentSearchState.searchResults" v-if="!loading"/>
   </SelectionSidebar>
-  <router-view @saved="fetchData"></router-view>
+  <router-view @saved="fetchSearch(this.componentSearchState.key)" @deleted="onItemDeleted"></router-view>
 </template>
 
 <script>
 import {ComponentFetcher} from "@/utils/ComponentFetcher";
 import ComponentTable from "@/components/imacs_component/ComponentTable";
 import SelectionSidebar from "@/components/util/selection_sidebar/SelectionSidebar";
+import {componentSearchState} from "@/components/imacs_component/Component";
 
 export default {
   name: "ComponentsView",
@@ -23,19 +25,20 @@ export default {
     return {
       loading: false,
       componentList: null,
-      error: null
+      error: null,
+      componentSearchState
     }
   },
   created() {
-    this.$watch(
-        () => this.$route.params,
-        () => {
-          this.fetchData()
-        },
-        {
-          immediate: true
-        }
-    )
+
+    if (componentSearchState.fresh) {
+      this.fetchSearch("")
+      componentSearchState.fresh = false
+    }
+
+    if (componentSearchState.lastVisitedId != null) {
+      this.$router.push({ name: 'compType', query: { id: componentSearchState.lastVisitedId, viewMode: 'change' }})
+    }
   },
   methods: {
     fetchData() {
@@ -55,13 +58,15 @@ export default {
           })
     },
     fetchSearch(key) {
+      this.componentSearchState.key = key
+
       this.error = this.post = null
       this.loading = true
 
       ComponentFetcher.searchForKey(key)
           .then(response => {
             response.json().then(data => {
-              this.componentList = data
+              this.componentSearchState.searchResults = data
               this.loading = false
             })
           })
@@ -69,6 +74,9 @@ export default {
             this.error = error
             this.loading = false
           })
+    },
+    onItemDeleted() {
+      this.fetchSearch(this.componentTypesSearchState.key)
     }
   }
 }
