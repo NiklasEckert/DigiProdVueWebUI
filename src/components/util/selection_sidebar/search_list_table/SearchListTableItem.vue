f6ca7f5f0671281e64b7b91223cbabf926c9b8dc
niklaseckert <niklas.eckert.1807@gmail.com>
2022-07-07 10:24:16 +0200

    Add: Refactored ComponentTypesTable into separate components.

---
+++ src/components/util/selection_sidebar/search_list_table/SearchListTableItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <td class="p-3 first:rounded-l last:rounded-r">{{ value }}</td>
+</template>
+
+<script>
+export default {
+  name: "SearchListTableItem",
+  props: ['value']
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/util/selection_sidebar/search_list_table/SearchListTableRow.vue
@@ -0,0 +1,21 @@
+<template>
+  <tr
+      :href="href"
+      @click="navigate"
+      class="hover:bg-gray-100 transition duration-100 hover:cursor-pointer"
+      :class="{ 'bg-amber-200': isActive }"
+  >
+    <slot></slot>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "SearchListTableRow",
+  props: ['href', 'navigate', 'isActive']
+}
+</script>
+
+<style scoped>
+
+</style>
