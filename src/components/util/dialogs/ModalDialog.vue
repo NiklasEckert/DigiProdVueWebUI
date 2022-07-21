c31a8b6040676283870fe38281f4bf6e275165d7
niklaseckert <niklas.eckert.1807@gmail.com>
2022-07-21 18:54:39 +0200

    Change: Modified ModalDialog to have a additional section for custom functionallity.

--- src/components/util/dialogs/ModalDialog.vue
+++ src/components/util/dialogs/ModalDialog.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="fixed top-0 right-0 bottom-0 left-0 bg-gray-400/50 flex justify-center items-center backdrop-blur-sm">
-    <div class="p-6 bg-white rounded-md box-border shadow-2xl scoped-min-w-96">
-      <div class="font-bold">{{ this.title }}</div>
+  <div class="fixed top-0 right-0 bottom-0 left-0 bg-gray-400/50 flex justify-center items-center z-50 backdrop-blur-sm">
+    <div class="bg-white rounded-md box-border shadow-2xl scoped-min-w-96 z-10">
+      <div class="px-6 pt-4">
+        <div class="font-bold">{{ this.title }}</div>
 
-      <div class="mt-2">
-        <p>{{ this.hint }}</p>
+        <div class="mt-2">
+          <p>{{ this.hint }}</p>
+        </div>
       </div>
 
-      <div class="flex flex-row justify-end mt-6">
+      <div>
+        <slot name="additionalFunction">
+
+        </slot>
+      </div>
+
+      <div class="flex flex-row justify-end mt-6 px-6 pb-4 bg-white rounded-b-md">
         <slot name="buttonRow">
 
         </slot>
